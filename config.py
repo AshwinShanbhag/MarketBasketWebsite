aa41b269e0fea95c92bc367aece71085cb5aeb01
Abhishek Reddy <82632323+abhishekkreddy@users.noreply.github.com>
2023-02-27 17:39:29 +0530

    deleted config

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///myapp.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
